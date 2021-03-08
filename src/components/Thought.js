db9ea24eb37091d2c82e15b9b2a90b476cfb639a
alicefritz <alicejfritz@gmail.com>
2021-03-08 10:39:07 +0100

    Thought component updates

--- src/components/Thought.js
+++ src/components/Thought.js
@@ -8,6 +8,7 @@ import Comments from './Comments'
 export default function Thought({thought}) {
   const [addCommentOpen, setAddCommentOpen] = useState(false)
   const [commentsOpen, setCommentsOpen] = useState(false)
+  const [comments, setComments] = useState([])
   
 
   const toggleAddComment = () => {
@@ -19,18 +20,20 @@ export default function Thought({thought}) {
     setCommentsOpen(!commentsOpen)
   }
 
+  console.log(comments.length)
+
   return (
     <div className="thought">
       <h3>{thought.thinker}</h3>
       <p>{thought.thought}</p>
-      <p>{thought.time ? thought.time.slice(0, -14) : 'just now'}</p>
-      <p>{thought.comment_count ? thought.comment_count : 0} comments</p>
+      <p>{thought.time.length > 10 ? thought.time.slice(0, 10) : thought.time}</p>
+      <p>{comments.length ? comments.length : thought.comment_count ? thought.comment_count : 0} comments</p>
       <div className="buttons">
-        {thought.comment_count != 0 && <Button text={commentsOpen ? 'Hide comments' : 'Read comments'} toggle={toggleComments}/>}
+        {(thought.comment_count > 0 || comments.length > 0) && <Button text={commentsOpen ? 'Hide comments' : 'Read comments'} toggle={toggleComments}/>}
         <Button text={addCommentOpen ? 'Close' : 'Add comment'}  toggle={toggleAddComment}/>
       </div>
-      {commentsOpen && <Comments ThoughtID={thought.ThoughtID}/>}
-      {addCommentOpen && <AddComment thought={thought}/>}
+      {commentsOpen && <Comments ThoughtID={thought.ThoughtID} comments={comments} setComments={setComments}/>}
+      {addCommentOpen && <AddComment thought={thought} setComments={setComments} comments={comments}/>}
     </div>
   )
 }
