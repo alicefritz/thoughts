52975926b3979a57010b04eef584f63402d9bc28
alicefritz <alicejfritz@gmail.com>
2021-03-10 10:40:46 +0100

    thoughts now get a random color on first render

--- src/components/Thought.js
+++ src/components/Thought.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Thought.css'
 import AddComment from './AddComment'
 import Button from './Button'
@@ -9,6 +9,7 @@ export default function Thought({thought}) {
   const [addCommentOpen, setAddCommentOpen] = useState(false)
   const [commentsOpen, setCommentsOpen] = useState(false)
   const [comments, setComments] = useState([])
+  const [color, setColor] = useState("");
   
 
   const toggleAddComment = () => {
@@ -19,8 +20,18 @@ export default function Thought({thought}) {
     setCommentsOpen(!commentsOpen)
   }
 
+  const getRandomColor = () => {
+    const opacity = 0.6;
+    const colors = [`rgba(132, 255, 255, ${opacity})`, `rgba(179, 136, 255, ${opacity})`, `rgba(204, 255, 144, ${opacity})`, `rgba(255, 234, 0, ${opacity})`, `rgba(248, 187, 208, ${opacity})`];
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  useEffect(() => {
+    setColor(getRandomColor());
+  }, [])
+
   return (
-    <div className="thought">
+    <div className="thought" style={{backgroundColor: color}}>
       <h3>{thought.thinker}</h3>
       <p>{thought.thought}</p>
       <p>{thought.time.length > 10 ? thought.time.slice(0, 10) : thought.time}</p>
